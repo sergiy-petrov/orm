1c3bfc2cc01918645f8c281baa6d97cfb7c8e022
Tom H Anderson <tom.h.anderson@gmail.com>
2024-10-26 19:40:08 -0600

    Added namespace to Extensions test

---
+++ tests/Assets/Extensions/ExtensionMock.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace LaravelDoctrineTest\ORM\Assets\Extensions;
+
+use Doctrine\Common\EventManager;
+use Doctrine\ORM\EntityManagerInterface;
+use LaravelDoctrine\ORM\Extensions\Extension;
+use LaravelDoctrineTest\ORM\Feature\Extensions\ExtensionManagerTest;
+
+class ExtensionMock implements Extension
+{
+    public function addSubscribers(EventManager $manager, EntityManagerInterface $em): void
+    {
+        // Confirm it gets called
+        (new ExtensionManagerTest)->assertTrue(true);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function getFilters(): array
+    {
+        return [];
+    }
+}
